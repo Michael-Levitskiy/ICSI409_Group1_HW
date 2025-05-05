8b7beb8d53006cba21a4a0c4c191c9e64c853687
Michael Levitskiy <mlevitskiy6@gmail.com>
2025-05-05 16:54:02 -0400

    Added Methods to fill

--- CYK.java
+++ CYK.java
@@ -1,5 +1,45 @@
+import java.util.HashMap;
+
 public class CYK{
+    String input;
+    HashMap<String, String[]> Production_Rules;
+
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         
+    }
+
+
+    /**
+     * Get the input string to check
+     */
+    public static void stringInput(){
+
+    }
+
+    /**
+     * Get the Production rules for the CNF
+     */
+    public static void rulesInput(){
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static boolean CYK_Algorithm(){
+
+
+
+
+
+
+
+
+        return true;
     }
 }
